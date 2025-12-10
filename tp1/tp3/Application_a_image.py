cef41ceb11e6885d31ec16eb60cb99bd3aced92a
renaudin-clement <pro.renaudin.clement@gmail.com>
2025-12-10 14:52:22 +0100

    Exercice 4. tp3

--- tp1/tp3/Application_a_image.py
+++ tp1/tp3/Application_a_image.py
@@ -5,6 +5,23 @@ from scipy import signal
 from matplotlib.pyplot import imshow, get_cmap
 import matplotlib.pyplot as plt
 
+
+import numpy as np
+from skimage import data
+
+import os
+import sys
+sys.path.append(os.path.expanduser("~/py-packages/"))
+
+
+from skimage.measure import block_reduce
+import matplotlib.pyplot as plt
+
+
+
+
+
+
 def displayTwoBaWImages(img1, img2):
     _, axes = plt.subplots(ncols=2)
     axes[0].imshow(img1, cmap=plt.get_cmap('gray'))
@@ -18,13 +35,21 @@ image = np.array([
     [0,0,0,0,0]
 ])
 
+image = data.horse()
+
+filtre =[
+    [0,1,0],
+    [1,-4,1],
+    [0,1,0]
+]
+
 
 cmp = get_cmap('gray')
 imshow(image, cmap=cmp, vmin=0, vmax=1)
 plt.show()
 
 
-kernel = np.ones((3,3), np.float32)/2
+kernel = filtre
 print(kernel)
 
 imgconvol = signal.convolve2d(image,
@@ -32,6 +57,10 @@ imgconvol = signal.convolve2d(image,
         mode='same',
         boundary='fill',
         fillvalue=0)
-displayTwoBaWImages(image, imgconvol)
+
+
+
+
+displayTwoBaWImages(image,imgconvol )
 
 plt.show()
